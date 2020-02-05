1d49df3f5c389cc1ce76de71d318301572f417d3
AwrenNuit <a.rennaker@yahoo.com>
2020-02-04 21:31:49 -0600

    formatted starting HTML

--- src/components/PasswordGenerator/PasswordGenerator.js
+++ src/components/PasswordGenerator/PasswordGenerator.js
@@ -12,36 +12,42 @@ export default function PasswordGenerator() {
   return(
     <div id="full-container">
       <h1>Password Generator</h1>
+
       <input type="checkbox" id="up-alphabet" />
-        <span>Add uppercase?</span>
-        <br />
+      <span>Add uppercase?</span>
+      <div>
         <textarea id="up-alphabet-display" readonly>ABCDEFGHIJKLMNOPQRSTUVWXYZ</textarea>
-        <br />
-        <hr />
+      </div>
+      <hr />
+
       <input type="checkbox" id="lo-alphabet" checked />
-        <span>Add lowercase?</span>
-        <br />
+      <span>Add lowercase?</span>
+      <div>
         <textarea id="lo-alphabet-display" readonly>abcdefghijklmnopqrstuvwxyz</textarea>
-        <br />
-        <hr />
+      </div>
+      <hr />
+
       <input type="checkbox" id="numbers" />
-        <span>Add numbers?</span>
-        <br />
+      <span>Add numbers?</span>
+      <div>
         <textarea id="numbers-display" readonly>1234567890</textarea>
-        <br />
-        <hr />
+      </div>
+      <hr />
+
       <input type="checkbox" id="dash" />
-        <span>Add dash & underscore?</span>
-        <br />
+      <span>Add dash & underscore?</span>
+      <div>
         <textarea id="dash-display" readonly>-_</textarea>
-        <br />
-        <hr />
+      </div>
+      <hr />
+
       <input type="checkbox" id="symbols" />
-        <span>Add other symbols?</span>
-        <br />
+      <span>Add other symbols?</span>
+      <div>
         <textarea id="symbols-display" readonly>#;:`~!@#$%^&*()+={}[]/?\</textarea>
-        <br />
-        <hr />
+      </div>
+      <hr />
+
       <input 
         type="number" 
         min="2" 
@@ -49,9 +55,11 @@ export default function PasswordGenerator() {
         value="10" 
         id="length"
         onChange={(event)=>setLength(event.target.value)} />
-      <br />
-      <button id="generate">Generate</button>
+      <div>
+        <button id="generate">Generate</button>
+      </div>
       <hr />
+      
       <div>
         <p id="output">Your unique ID is: </p>
       </div>
