7758ab4f416eb15249865911f171c0a807c6b5b7
AwrenNuit <a.rennaker@yahoo.com>
2020-02-22 13:11:29 -0600

    set up component formatting, imported into App.js

--- src/components/Abstract/Abstract.js
+++ src/components/Abstract/Abstract.js
@@ -1,52 +1,74 @@
-import React, {} from 'react';
-import 'Abstract.css';
+import React from 'react';
+import './Abstract.css';
 
 export default function Abstract() {
 
     return(
-      <>
-        <h1>Choose Your Palette</h1>
-        <div class="row">
-            <div class="column">
-                <input type="radio" id="dark" name="colors" />  
-                <label>Dark</label>
-            </div>
-            <div class="column">
-                <input type="radio" id="colorful" name="colors" />
-                <label>Colorful</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="column">
-                <input type="radio" id="darker" name="colors" />
-                <label>Darker</label>
-            </div>
-            <div class="column">
-                <input type="radio" id="vaporwave" name="colors" />
-                <label>Vaporwave</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="column">
-                <input type="radio" id="darkest" name="colors" />
-                <label>Darkest</label>
-            </div>
-            <div class="column">
-                <input type="radio" id="neon" name="colors" />
-                <label>Neon</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="column">
-                <button id="sad-btn"><span class="one">&#187;</span><span class="two">&#187;</span><span class="three">&#187;</span><span class="four">&#187;</span>ACCEPT SADNESS.<span class="four">&#171;</span><span class="three">&#171;</span><span class="two">&#171;</span><span class="one">&#171;</span></button>
-            </div>
-            <div class="column">
-                <button id="psych-btn"><span class="one">&#187;</span><span class="two">&#187;</span><span class="three">&#187;</span><span class="four">&#187;</span>ACCEPT PSYCHEDELICS.<span class="four">&#171;</span><span class="three">&#171;</span><span class="two">&#171;</span><span class="one">&#171;</span></button>
-            </div>
-        </div>
-        <div id="output">
+      <div className="abstract-container">
+        <h1 className="abstract-heading">Choose Your Palette</h1>
+        <center>
+          <div className="abstract-row">
+              <div className="abstract-column">
+                  <input type="radio" id="dark" name="colors" />  
+                  <label className="abstract-label">Dark</label>
+              </div>
+              <div className="abstract-column">
+                  <input type="radio" id="colorful" name="colors" />
+                  <label className="abstract-label">Colorful</label>
+              </div>
+          </div>
+          <div className="abstract-row">
+              <div className="abstract-column">
+                  <input type="radio" id="darker" name="colors" />
+                  <label className="abstract-label">Darker</label>
+              </div>
+              <div className="abstract-column">
+                  <input type="radio" id="vaporwave" name="colors" />
+                  <label className="abstract-label">Vaporwave</label>
+              </div>
+          </div>
+          <div className="abstract-row">
+              <div className="abstract-column">
+                  <input type="radio" id="darkest" name="colors" />
+                  <label className="abstract-label">Darkest</label>
+              </div>
+              <div className="abstract-column">
+                  <input type="radio" id="neon" name="colors" />
+                  <label className="abstract-label">Neon</label>
+              </div>
+          </div>
+          <div className="abstract-row">
+              <div className="abstract-column">
+                  <button className="abstract-btn" id="sad-btn">
+                    <span className="one abstract-span">&#187;</span>
+                    <span className="two abstract-span">&#187;</span>
+                    <span className="three abstract-span">&#187;</span>
+                    <span className="four abstract-span">&#187;</span>
+                    ACCEPT SADNESS.
+                    <span className="four abstract-span">&#171;</span>
+                    <span className="three abstract-span">&#171;</span>
+                    <span className="two abstract-span">&#171;</span>
+                    <span className="one abstract-span">&#171;</span>
+                  </button>
+              </div>
+              <div className="abstract-column">
+                  <button className="abstract-btn" id="psych-btn">
+                    <span className="one abstract-span">&#187;</span>
+                    <span className="two abstract-span">&#187;</span>
+                    <span className="three abstract-span">&#187;</span>
+                    <span className="four abstract-span">&#187;</span>
+                    ACCEPT PSYCHEDELICS.
+                    <span className="four abstract-span">&#171;</span>
+                    <span className="three abstract-span">&#171;</span>
+                    <span className="two abstract-span">&#171;</span>
+                    <span className="one abstract-span">&#171;</span>
+                  </button>
+              </div>
+          </div>
+        </center>
+        <div className="abstract-output">
             
         </div>
-      </>
+      </div>
     );
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,14 @@ import Header from '../Header/Header';
 import Home from '../Home/Home';
 import PasswordGenerator from '../PasswordGenerator/PasswordGenerator';
 import Resume from '../Resume/Resume';
+import Abstract from '../Abstract/Abstract';
 
 export default function App() {
   return (
     <Router>
       <Header />
       <Route exact path='/' component={Home} />
+      <Route exact path='/abstract-art' component={Abstract} />
       <Route exact path='/password-generator' component={PasswordGenerator} />
       <Route exact path='/resume' component={Resume} />
     </Router>
