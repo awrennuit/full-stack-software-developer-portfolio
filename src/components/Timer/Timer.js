fec668d7cb8927f6aeb735db1c1463f11a966e8f
AwrenNuit <a.rennaker@yahoo.com>
2020-04-26 13:31:44 -0500

    imported component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,122 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
 // import Button from '@material-ui/core/Button';
 
-export default function Timer() {
+class Timer extends Component{
 
-  const history = useHistory();
-  const timer = useSelector(state => state.timer);
-  const [alarm, setAlarm] = useState(false);
-  const [alarmDone, setAlarmDone] = useState(false);
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  // const [interval, setInterval] = useState('');
-
-  useEffect(()=>{
-    set();
-  }, []);
-
-  async function set() {
-    await setDays(+timer.days);
-    await setHours(+timer.hours);
-    await setMinutes(+timer.minutes);
-    await setSeconds(+timer.seconds);
-    setInterval(() => countdown(), 1000);
+  state = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    alarm: false,
+    alarmDone: false
   }
 
-  useEffect(()=>{
-    if(alarm){
-      const audio = new Audio(`/sounds/alarm.mp3`);
+  UNSAFE_componentWillMount(){
+    this.setState(this.props.state);
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(() => this.countdown(), 1000);
+  }
+
+  componentDidUpdate(){
+    if(this.state.alarm){
+      let audio = new Audio('/sounds/alarm.mp3');
       audio.play();
-      setAlarm(false);
-      setAlarmDone(true);
+      this.setState({
+        alarm: false,
+        alarmDone: true
+      });
     }
-  }, [alarm]);
+  }
 
-  // useEffect(()=>{
-  //   clearInterval(interval);
-  // }, [history]);
+  componentWillUnmount(){
+    clearInterval(this.interval);
+  }
 
-  const countdown = () => {
+  countdown = () => {
 
     const calcTimeLeft = () => {
+      let remaining = {};
 
-      let newDays = +days;
-      let newHours = +hours;
-      let newMinutes = +minutes;
-      let newSeconds = +seconds;
-      console.log('sec:', seconds);
-      console.log('newsec:', newSeconds);
+      let days = this.state.days;
+      let hours = this.state.hours;
+      let minutes = this.state.minutes;
+      let seconds = this.state.seconds;
 
-      // newSeconds--;
+      seconds--;
 
       // Handle seconds change
-      // if(newSeconds <= 0 && newMinutes <= 0 && newHours <= 0 && newDays <= 0){
-      //   newSeconds = 0;
-      //   if(!alarmDone){
-      //     setAlarm(true);
-      //   }
-      // }
-      // else if(newSeconds < 0){
-      //   newSeconds = 59;
-      //   newMinutes--;
-      // }
+      if(seconds <= 0 && minutes <= 0 && hours <= 0 && days <= 0){
+        seconds = 0;
+        clearInterval(this.interval);
+        if(!this.state.alarmDone){
+          this.setState({alarm: true});
+        }
+      }
+      else if(seconds < 0){
+        seconds = 59;
+        minutes--;
+      }
 
-      // // Handle minutes change
-      // if(newMinutes <= 0 && newHours <= 0 && newDays <= 0){
-      //   newMinutes = 0;
-      // }
-      // else if(newSeconds === 59 && newMinutes <= 0){
-      //   newMinutes = 59;
-      //   newHours--;
-      // }
+      // Handle minutes change
+      if(minutes <= 0 && hours <= 0 && days <= 0){
+        minutes = 0;
+      }
+      else if(seconds === 59 && minutes <= 0){
+        minutes = 59;
+        hours--;
+      }
 
-      // // Handle hours change
-      // if(newHours <= 0 && newDays <= 0){
-      //   newHours = 0;
-      // }
-      // else if(newSeconds === 59 && newMinutes === 59 && newHours <= 0){
-      //   newHours = 23;
-      //   newDays--;
-      // }
+      // Handle hours change
+      if(hours <= 0 && days <= 0){
+        hours = 0;
+      }
+      else if(seconds === 59 && minutes === 59 && hours <= 0){
+        hours = 23;
+        days--;
+      }
 
-      setDays(newDays);
-      setHours(newHours);
-      setMinutes(newMinutes);
-      setSeconds(newSeconds);
+      remaining = {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+      }
+
+      this.setState(remaining);
 
         // ------------------TO USE DATE AS COUNTDOWN-----------------
       // let stop = new Date('2020-01-01');
-      // stop.setDate(stop.getDate()+days);
-      // stop.setHours(stop.getHours()+hours);
-      // stop.setMinutes(stop.getMinutes()+minutes);
-      // stop.setSeconds(stop.getSeconds()+seconds);
+      // stop.setDate(stop.getDate()+this.state.days);
+      // stop.setHours(stop.getHours()+this.state.hours);
+      // stop.setMinutes(stop.getMinutes()+this.state.minutes);
+      // stop.setSeconds(stop.getSeconds()+this.state.seconds);
       // let now = new Date();
+      // let remaining={};
       // let timeDiff = (stop - now) / 1000;
       // if (timeDiff > 0) {
-      //     setDays(Math.floor(timeDiff / 86400));
-      //     setHours(Math.floor((timeDiff % 86400) / 3600));
-      //     setMinutes(Math.floor((timeDiff % 3600) / 60));
-      //     setSeconds(Math.floor(timeDiff % 60));
+      //   remaining = {
+      //     days: Math.floor(timeDiff / 86400),
+      //     hours: Math.floor((timeDiff % 86400) / 3600),
+      //     minutes: Math.floor((timeDiff % 3600) / 60),
+      //     seconds: Math.floor(timeDiff % 60)
+      //   };
+      // this.setState(remaining);
       // }
     }
     setInterval(calcTimeLeft(), 1000);
   }
 
-  return(
-    <div className="countdown-container">
-      {JSON.stringify(seconds)}
-      <div className="countdown">
-        {days < 10 ? "0"+days : days}d:
-        {hours < 10 ? "0"+hours : hours}h:
-        {minutes < 10 ? "0"+minutes : minutes}m:
-        {seconds < 10 ? "0"+seconds : seconds}s
-      </div>
-      {/* <Button variant="outlined" onClick={setTimer}>Set Timer</Button> */}
-      <button onClick={()=>history.push(`/set-timer`)}>Set Timer</button>
-    </div>
-  );
+  render(){
+    return(
+      <>
+        <div className="countdown-container">
+          <div className="countdown">
+            {this.state.days < 10 ? "0"+this.state.days : this.state.days}d:
+            {this.state.hours < 10 ? "0"+this.state.hours : this.state.hours}h:
+            {this.state.minutes < 10 ? "0"+this.state.minutes : this.state.minutes}m:
+            {this.state.seconds < 10 ? "0"+this.state.seconds : this.state.seconds}s
+          </div>
+
+          {/* <Link to="/"> */}
+            {/* <Button variant="outlined">Set Timer</Button> */}
+            <button onClick={()=>this.props.history.push(`/set-timer`)}>Set Timer</button>
+          {/* </Link> */}
+
+        </div>
+      </>
+    );
+  }
 }
+
+const reduxState = state => ({state: state.timer});
+
+export default connect(reduxState)(Timer);
