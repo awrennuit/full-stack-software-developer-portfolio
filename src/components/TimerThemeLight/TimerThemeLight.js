945fc638d676061bd62d2e8e3d46e937ba70e052
AwrenNuit <a.rennaker@yahoo.com>
2020-04-26 13:44:17 -0500

    minor formatting updates

--- src/components/TimerThemeLight/TimerThemeLight.js
+++ src/components/TimerThemeLight/TimerThemeLight.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-const ToggleThemeLight = ({ onToggleTheme }) => (
-  <Button variant="outlined" onClick={onToggleTheme} size="small" style={{float:"right",margin:"30px"}}>
+export const ToggleThemeLight = ({ onToggleTheme }) => (
+  <Button 
+    variant="outlined" 
+    onClick={onToggleTheme} 
+    size="small" 
+    style={{float:"right",margin:"30px"}}
+  >
     Dark Mode
   </Button>
 );
