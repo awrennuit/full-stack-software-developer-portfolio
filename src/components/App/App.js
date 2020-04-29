569b7c8ca85e9e2e8ef07c5fca7a432b14f13fd8
AwrenNuit <a.rennaker@yahoo.com>
2020-04-29 16:51:05 -0500

    created component, set up basic template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-// import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Abstract from '../Abstract/Abstract';
+import CssSandbox from '../CssSandbox/CssSandbox';
 import Landing from '../Landing/Landing';
 import PasswordGenerator from '../PasswordGenerator/PasswordGenerator';
 import Resume from '../Resume/Resume';
@@ -13,10 +13,10 @@ import Timer from '../Timer/Timer';
 export default function App() {
   return (
     <Router>
-      {/* <Header /> */}
       <Route exact path='/' component={Landing} />
       <Route exact path='/main' component={Home} />
       <Route exact path='/abstract-art' component={Abstract} />
+      <Route exact path='/css-sandbox' component={CssSandbox} />
       <Route exact path='/password-generator' component={PasswordGenerator} />
       <Route exact path='/resume' component={Resume} />
       <Route exact path='/set-timer' component={TimerMain} />
---
+++ src/components/CssSandbox/CssSandbox.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import './CssSandbox.css';
+
+export default function CssSandbox() {
+
+  // sets background color
+  const [bgRed, setBgRed] = useState(255);
+  const [bgGreen, setBgGreen] = useState(255);
+  const [bgBlue, setBgBlue] = useState(255);
+  const [bgColor, setBgColor] = useState(`rgb(${bgRed}, ${bgGreen}, ${bgBlue})`);
+
+  //sets border
+  const [borderWidth, setBorderWidth] = useState(0);
+  const [borderType, setBorderType] = useState('solid');
+  const [borderRed, setBorderRed] = useState(125);
+  const [borderGreen, setBorderGreen] = useState(125);
+  const [borderBlue, setBorderBlue] = useState(125);
+  const [borderColor, setBorderColor] = useState(`rgb(${borderRed}, ${borderGreen}, ${borderBlue})`);
+
+  // sets border radius
+  const [borderRadius, setBorderRadius] = useState(0);
+  const [borderRadiusType, setBorderRadiusType] = useState('px');
+
+  // sets box shadow
+  const [boxRed, setBoxRed] = useState(125);
+  const [boxGreen, setBoxGreen] = useState(125);
+  const [boxBlue, setBoxBlue] = useState(125);
+  const [boxColor, setBoxColor] = useState(`rgb(${boxRed}, ${boxGreen}, ${boxBlue})`);
+  const [boxInset, setBoxInset] = useState('');
+  const [boxOffsetX, setBoxOffsetX] = useState(0);
+  const [boxOffsetY, setBoxOffsetY] = useState(0);
+  const [boxBlur, setBoxBlur] = useState(0);
+  const [boxSpread, setBoxSpread] = useState(0);
+
+  useEffect(()=>{
+    setBgColor(`rgb(${bgRed}, ${bgGreen}, ${bgBlue})`);
+  }, [bgRed, bgGreen, bgBlue]);
+
+  useEffect(()=>{
+    setBorderColor(`rgb(${borderRed}, ${borderGreen}, ${borderBlue})`);
+  }, [borderRed, borderGreen, borderBlue]);
+
+  useEffect(()=>{
+    setBoxColor(`rgb(${boxRed}, ${boxGreen}, ${boxBlue})`);
+  }, [boxRed, boxGreen, boxBlue]);
+
+  return(
+    <div className="box-shadow-container">
+
+      <div 
+        className="test-div"
+        style={{
+          backgroundColor:bgColor,
+          border:`${borderWidth}px solid ${borderColor}`,
+          borderRadius:`${borderRadius}${borderRadiusType}`,
+          boxShadow:`${boxInset} ${boxOffsetX}px ${boxOffsetY}px ${boxBlur}px ${boxSpread}px ${boxColor}`,
+          height:"200px",
+          margin:"20% auto 0 auto",
+          width:"350px"
+        }}
+      >
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>
+        <p>background color</p>
+        red <input type="range" value={bgRed} min={0} max={255} step={1} onChange={e=>setBgRed(e.target.value)} />
+        <br />
+        green <input type="range" value={bgGreen} min={0} max={255} step={1} onChange={e=>setBgGreen(e.target.value)} />
+        <br />
+        blue <input type="range" value={bgBlue} min={0} max={255} step={1} onChange={e=>setBgBlue(e.target.value)} />
+      </div>
+      <br />
+      <br />
+      <div>
+        <p>border</p>
+        width <input type="range" value={borderWidth} min={0} max={50} step={1} onChange={e=>setBorderWidth(e.target.value)} />
+        <br />
+        {/* type <input type="range" value={borderType} min={0} max={255} step={1} onChange={e=>setBorderType(e.target.value)} /> */}
+        <br />
+        red <input type="range" value={borderRed} min={0} max={255} step={1} onChange={e=>setBorderRed(e.target.value)} />
+        <br />
+        green <input type="range" value={borderGreen} min={0} max={255} step={1} onChange={e=>setBorderGreen(e.target.value)} />
+        <br />
+        blue <input type="range" value={borderBlue} min={0} max={255} step={1} onChange={e=>setBorderBlue(e.target.value)} />
+      </div>
+      <br />
+      <br />
+      <div>
+        <p>border radius</p>
+        width <input type="range" value={borderRadius} min={0} max={100} step={1} onChange={e=>setBorderRadius(e.target.value)} />
+        <br />
+        px <input type="radio" name="borderRadiusType" value='px' onChange={e=>setBorderRadiusType(e.target.value)} checked />
+        <br />
+        % <input type="radio" name="borderRadiusType" value='%' onChange={e=>setBorderRadiusType(e.target.value)} />
+      </div>
+      <br />
+      <br />
+      <div>
+        <p>box shadow color</p>
+        box red<input type="range" value={boxRed} min={0} max={255} step={1} onChange={e=>setBoxRed(e.target.value)} />
+        <br />
+        box green<input type="range" value={boxGreen} min={0} max={255} step={1} onChange={e=>setBoxGreen(e.target.value)} />
+        <br />
+        box blue<input type="range" value={boxBlue} min={0} max={255} step={1} onChange={e=>setBoxBlue(e.target.value)} />
+      </div>
+      <br />
+      <br />
+      <div>
+        <p>box shadow parameters</p>
+        inset <input type="checkbox" onChange={e=>{e.target.checked ? setBoxInset('inset') : setBoxInset('')}} />
+        <br />
+        x offset <input type="range" value={boxOffsetX} min={-100} max={100} step={1} onChange={e=>setBoxOffsetX(e.target.value)} />
+        <br />
+        y offset <input type="range" value={boxOffsetY} min={-100} max={100} step={1} onChange={e=>setBoxOffsetY(e.target.value)} />
+        <br />
+        blur <input type="range" value={boxBlur} min={0} max={100} step={1} onChange={e=>setBoxBlur(e.target.value)} />
+        <br />
+        spread <input type="range" value={boxSpread} min={-100} max={100} step={1} onChange={e=>setBoxSpread(e.target.value)} />
+      </div>
+    </div>
+  );
+}
