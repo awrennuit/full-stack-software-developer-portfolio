bb40a6858a495c3fdaa4ef17ed62eeb59806976e
AwrenNuit <a.rennaker@yahoo.com>
2020-05-02 19:30:16 -0500

    created stylesheet, increased heading size, added placeholder border, adjusted margins and line height

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,37 +1,41 @@
 import React from 'react';
+import './About.css';
 
 export default function About() {
 
   return (
-    <div>
-      <h3>A little history...</h3>
-      <p>
-        Growing up I was always coming up with new ideas and figuring out how to bring them
-        to life. As I got older this manifested through visual arts and music. I performed 
-        with the school orchestra all through middle and high school, and have taught myself 
-        dozens of instruments since then.
-      </p>
-      <p>
-        I also have an affinity for plants, which blossomed into a deep study of Western 
-        herbalism. Forming a bond with the plants that I use is very important to me, so 
-        I raise many of them from seed so I can study them and be present with them through 
-        their entire lifecycle from seed to sprout to bloom and back again to seed. Ask me 
-        about my cough syrup sometime - it's delicious and nutritious!
-      </p>
-      <p>
-        My passions for creating and understanding how things work on a fundamental level 
-        carry over into my software development. I have a profound appreciation for engaging, 
-        interactive, and accessible design. In addition, I love walking outside of my comfort 
-        zone to try new things and see what underlying processes they function with.
-      </p>
-      <br />
-      <p>
-        I'm always open for a conversation, so feel free to&nbsp;
-        <a href="mailto:awren.nuit@gmail.com?subject=Let's%20Have%20a%20Conversation">
-          contact me
-        </a>
-        .
-      </p>
+    <div className="about-main-container">
+      <h2>A little history...</h2>
+      <div className="about-content-container">
+        <p>
+          Growing up I was always coming up with new ideas and figuring out how to bring them
+          to life. As I got older this manifested through visual arts and music. I performed 
+          with the school orchestra all through middle and high school, and have taught myself 
+          dozens of instruments since then.
+        </p>
+        <p>
+          I also have an affinity for plants, which blossomed into a deep study of Western 
+          herbalism. Forming a bond with the plants that I use is very important to me, so 
+          I raise many of them from seed so I can study them and be present with them through 
+          their entire lifecycle from seed to sprout to bloom and back again to seed. Ask me 
+          about my cough syrup sometime - it's delicious and nutritious!
+        </p>
+        <p>
+          My passions for creating and understanding how things work on a fundamental level 
+          carry over into my software development. I have a profound appreciation for engaging, 
+          interactive, and accessible design. In addition, I love learning new technologies and 
+          walking outside my comfort zone. I embrace feeling uncomfortable, because that's 
+          where the majority of growth takes place.
+        </p>
+        <br />
+        <p>
+          I'm always open for a conversation, so feel free to&nbsp;
+          <a href="mailto:awren.nuit@gmail.com?subject=Let's%20Have%20a%20Conversation">
+            contact me
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
