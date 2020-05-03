9099cb37e8783d4a0cf68c17392d9a0617506b22
AwrenNuit <a.rennaker@yahoo.com>
2020-05-02 19:33:07 -0500

    imported Header

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Header from '../Header/Header';
 
 export default function Projects() {
 
   const history = useHistory();
   return(
     <>
-    <div className="project-card">
-      <img src="images/avatar.jpg" alt="react timer" width="300" />
-      <div className="project-overlay">
-        <h1>React Timer</h1>
-        <button onClick={()=>history.pushState('/timer')}>View Here</button>
+      <Header />
+      <div className="project-card">
+        <img src="images/avatar.jpg" alt="react timer" width="300" />
+        <div className="project-overlay">
+          <h1>React Timer</h1>
+          <button onClick={()=>history.pushState('/timer')}>View Here</button>
+        </div>
       </div>
-    </div>
     </>
   );
 }
