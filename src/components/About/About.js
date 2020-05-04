82f0ec2a3d90233e843f350d9a669f7fcbc90ab7
AwrenNuit <a.rennaker@yahoo.com>
2020-05-04 12:02:02 -0500

    updated language

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,13 +18,13 @@ export default function About() {
           herbalism. Forming a bond with the plants that I use is very important to me, so 
           I raise many of them from seed so I can study them and be present with them through 
           their entire lifecycle from seed to sprout to bloom and back again to seed. Ask me 
-          about my cough syrup sometime - it's delicious and nutritious!
+          about my homemade cough syrup sometime - it's delicious and nutritious!
         </p>
         <p>
           My passions for creating and understanding how things work on a fundamental level 
           carry over into my software development. I have a profound appreciation for engaging, 
           interactive, and accessible design. In addition, I love learning new technologies and 
-          walking outside my comfort zone. I embrace feeling uncomfortable, because that's 
+          working outside my comfort zone. I embrace feeling uncomfortable, because that's 
           where the majority of growth takes place.
         </p>
         <br />
