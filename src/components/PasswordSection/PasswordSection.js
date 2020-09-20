1846f35cd9373a5db97446c8050d05c3a140dcc9
AwrenNuit <a.rennaker@yahoo.com>
2020-09-20 10:24:19 -0500

    migrate each password option to utility component

---
+++ src/components/PasswordSection/PasswordSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../PasswordGenerator/PasswordGenerator.css";
+
+export default function PasswordSection(props) {
+  return (
+    <>
+      <div className="password-checkbox-container">
+        <input
+          type="checkbox"
+          value={props.value}
+          onChange={props.handleCheck}
+          defaultChecked={props.symbolType === "lowercase"}
+        />
+        <span className="password-span">Add {props.symbolType}?</span>
+      </div>
+      <div className="password-text-container">
+        <textarea
+          className="password-textarea"
+          value={props.value}
+          readOnly
+        ></textarea>
+      </div>
+      <hr className="password-hr" />
+    </>
+  );
+}
