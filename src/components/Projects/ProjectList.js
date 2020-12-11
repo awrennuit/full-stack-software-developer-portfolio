07e1ff122ffb3a13e989e1a5b558de7a48507c2d
AwrenNuit <a.rennaker@yahoo.com>
2020-12-11 14:33:17 -0600

    update file structure, consolidate project cards into a map, update ProjectCard url logic

---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Projects.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import ProjectCard from "./ProjectCard";
+import { allProjects } from "../../utils/allProjects";
+
+export default function Projects() {
+  const history = useHistory();
+
+  return (
+    <>
+      <Header />
+      <div className="project-container">
+        <div className="project-card-center">
+          {allProjects.map((card, i) => (
+            <ProjectCard
+              altText={card.altText}
+              appUrl={card.appUrl ? () => history.push(card.appUrl) : null}
+              codeUrl={card.codeUrl}
+              descriptionLineOne={card.descriptionLineOne}
+              descriptionLineTwo={card.descriptionLineTwo}
+              externalAppUrl={
+                card.externalAppUrl
+                  ? () => window.open(card.externalAppUrl, "_blank")
+                  : null
+              }
+              image={card.image}
+              key={i}
+              oneButton={card.oneButton}
+              title={card.title}
+            />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
