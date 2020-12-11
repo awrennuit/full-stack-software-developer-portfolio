374d25530d0e0b5924ae9b7ce635a671ffd9665e
AwrenNuit <a.rennaker@yahoo.com>
2020-12-11 15:24:56 -0600

    consolidate link info into a map, create component for header buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,92 +1,15 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { headerLinks } from "../../utils/headerLinks";
+import HeaderBtn from "./HeaderBtn";
+import "./Header.css";
 
 export default function Header() {
-
-  const history = useHistory();
-  const location = history.location.pathname;
-  // const [red, setRed] = useState(50);
-  // const [green, setGreen] = useState(50);
-  // const [blue, setBlue] = useState(65);
-  // const [bg, setBg] = useState('');
-
-  // useEffect(()=>{
-  //   document.body.style = `background: ${bg};`;
-  // }, [bg]);
-
-  // useEffect(()=>{
-  //   setBg(`rgb(${red}, ${green}, ${blue})`);
-  // }, [red, green, blue]);
-
-  // const changeBackground = dir => {
-  //   switch(dir){
-  //     case 'up':
-  //       if(blue < 255){
-  //         setRed(red + 5);
-  //         setGreen(green + 5);
-  //         setBlue(blue + 5);
-  //       }
-  //       break;
-  //     case 'down':
-  //       if(red > 0){
-  //         setRed(red - 5);
-  //         setGreen(green - 5);
-  //         setBlue(blue - 5);
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-
-  return(
+  return (
     <div className="header">
-      {/* <div>
-        <div style={{marginBottom:"10px"}}>
-          <button className="header-bg-btn" onClick={()=>changeBackground('up')}>&and;</button>
-        </div>
-        <div>
-          <button className="header-bg-btn" onClick={()=>changeBackground('down')}>&or;</button>
-        </div>
-      </div> */}
       <div className="header-flex">
-        <button 
-          className={location === '/about' ? 'header-btn-here' : 'header-btn'}
-          onClick={location !== '/about' ? ()=>(history.push('/about')) : null}>
-          <span className="header-btn-corner-one"></span>
-          <span className="header-btn-corner-two"></span>
-          About
-        </button>
-        <button 
-          className={location === '/projects' ? 'header-btn-here' : 'header-btn'} 
-          onClick={location !== '/projects' ? ()=>history.push('/projects') : null}
-        >
-          <span className="header-btn-corner-one"></span>
-          <span className="header-btn-corner-two"></span>
-          Projects
-        </button>
-        <button 
-          className={location === '/css-sandbox' ? 'header-btn-here' : 'header-btn'} 
-          onClick={location !== '/css-sandbox' ? ()=>history.push('/css-sandbox') : ''}
-        >
-          <span className="header-btn-corner-one"></span>
-          <span className="header-btn-corner-two"></span>
-          Sandbox
-        </button>
-        <button 
-          className={location === '/contact' ? 'header-btn-here' : 'header-btn'} 
-          onClick={location !== '/contact' ? ()=>history.push('/contact') : ''}
-        >
-          <span className="header-btn-corner-one"></span>
-          <span className="header-btn-corner-two"></span>
-          Contact
-        </button>
-        {/* <button className="header-btn" onClick={()=>window.location.href="mailto:awren.nuit@gmail.com?subject=I%20Loved%20Your%20Portfolio"}>
-          <span className="header-btn-corner-one"></span>
-          <span className="header-btn-corner-two"></span>
-          Email
-        </button> */}
+        {headerLinks.map((link, i) => (
+          <HeaderBtn key={i} label={link.label} url={link.url} />
+        ))}
       </div>
     </div>
   );
