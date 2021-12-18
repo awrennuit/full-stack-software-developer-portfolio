9d956fdda76429b3fb731b3de9d9e32572c98a1c
Awren Nuit <awren.nuit@gmail.com>
2021-12-18 14:13:06 -0600

    convert Sandbox output modal into its own component

--- src/components/CssSandbox/CssSandbox.js
+++ src/components/CssSandbox/CssSandbox.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import PropertyTab from './PropertyTab/PropertyTab';
 import TestDiv from './TestDiv/testDiv';
+import Output from './Output/Output';
 
 export default function CssSandbox() {
 
@@ -659,77 +660,28 @@ export default function CssSandbox() {
           </div>
         </div>
 
-        <div className="sandbox-modal-output" style={{opacity: showModal ? 1 : 0, transition: showModal ? 'opacity 0.3s' : '0.3s', visibility: showModal ? 'visible' : 'hidden'}} >
-          <h3>Here's Your CSS</h3>
-          <div 
-            className="sandbox-modal-output-content"
-            id="output"
-            onClick={()=>selectText('output')}
-          >
-            .my-class {'{'}
-              <br />
-              &nbsp;&nbsp;background-color: {bgColor};
-              {
-                +blur !== 0 ? 
-                  <>
-                    <br />
-                    &nbsp;&nbsp;blur: {blur}px;
-                  </> 
-                : 
-                  ''
-              }
-              {
-                +borderWidth !== 0 ? 
-                  <>
-                    <br />
-                    &nbsp;&nbsp;border: {borderWidth}px {borderStyle} {borderColor};
-                  </> 
-                : 
-                  ''
-              }
-              {
-                +borderRadius !== 0 ? 
-                  <>
-                    <br />
-                    &nbsp;&nbsp;border-radius: {borderRadius}{borderRadiusType};
-                  </> 
-                : 
-                  ''
-              }
-              {
-                +boxOffsetX !== 0 || +boxOffsetY !== 0 || +boxBlur !== 0 || +boxSpread !== 0 ? 
-                  <>
-                    <br />
-                    &nbsp;&nbsp;box-shadow: {boxInset} {boxOffsetX}px {boxOffsetY}px {boxBlur}px {boxSpread}px {boxColor};
-                  </> 
-                : 
-                  ''
-              }
-              <br />
-              &nbsp;&nbsp;height: {height}px;
-              {
-                +rotation !== 0 ? 
-                  <>
-                    <br />
-                    &nbsp;&nbsp;transform: rotate({rotation}deg);
-                  </> 
-                : 
-                  ''
-              }
-              <br />
-              &nbsp;&nbsp;width: {width}px;
-              <br />
-            {'}'}
-          </div>
-          <div style={{textAlign:"center"}}>
-            <button 
-              className="sandbox-btn-modal"
-              onClick={()=>setShowModal(!showModal)}
-            >
-              Got It
-            </button>
-          </div>
-        </div>
+        <Output
+          bgColor={bgColor}
+          blur={blur}
+          borderColor={borderColor}
+          borderRadius={borderRadius}
+          borderRadiusType={borderRadiusType}
+          borderStyle={borderStyle}
+          borderWidth={borderWidth}
+          boxBlur={boxBlur}
+          boxColor={boxColor}
+          boxInset={boxInset}
+          boxOffsetX={boxOffsetX}
+          boxOffsetY={boxOffsetY}
+          boxSpread={boxSpread}
+          height={height}
+          rotation={rotation}
+          selectText={selectText}
+          setShowModal={setShowModal}
+          showModal={showModal}
+          width={width}
+        />
+
       </div>
       <Footer />
     </>
