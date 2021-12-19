f55dc1d5bd3d249ef2e7c82a313dbf98112352cf
Awren Nuit <awren.nuit@gmail.com>
2021-12-19 14:30:34 -0600

    update Header btn onClick function

--- src/components/Header/HeaderBtn.js
+++ src/components/Header/HeaderBtn.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-export default function HeaderBtn(props) {
+export default function HeaderBtn({ label, url }) {
   const history = useHistory();
   const location = history.location.pathname;
 
   return (
     <button
-      className={location === props.url ? "header-btn-here" : "header-btn"}
-      onClick={location !== props.url ? () => history.push(props.url) : ""}
+      className={location === url ? "header-btn-here" : "header-btn"}
+      onClick={() => (location !== url ? history.push(url) : null)}
     >
       <span className="header-btn-corner-one"></span>
       <span className="header-btn-corner-two"></span>
-      {props.label}
+      {label}
     </button>
   );
 }
