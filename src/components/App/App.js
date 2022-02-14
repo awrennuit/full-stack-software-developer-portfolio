a75d62b399590544ff0ba9719ebff99e4929a447
AwrenNuit <a.rennaker@yahoo.com>
2022-02-14 17:17:33 -0600

    lazy load components; set explicit width for Skills imgs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Home from "../Home/Home";
-import Abstract from "../Abstract/Abstract";
-import Blog from "../Blog/Blog";
-import Contact from "../Contact/Contact";
-import CssSandbox from "../CssSandbox/CssSandbox";
-import CustomGradient from "../CustomGradient/CustomGradient";
-import Landing from "../Landing/Landing";
-import PasswordGenerator from "../PasswordGenerator/PasswordGenerator";
-import ProjectList from "../Projects/ProjectList";
-import Swatches from "../Swatches/Swatches";
-import TimerMain from "../TimerMain/TimerMain";
+
+const Abstract = React.lazy(() => import("../Abstract/Abstract"));
+const Blog = React.lazy(() => import("../Blog/Blog"));
+const Contact = React.lazy(() => import("../Contact/Contact"));
+const CssSandbox = React.lazy(() => import("../CssSandbox/CssSandbox"));
+const CustomGradient = React.lazy(() => import("../CustomGradient/CustomGradient"));
+const Home = React.lazy(() => import("../Home/Home"));
+const Landing = React.lazy(() => import("../Landing/Landing"));
+const PasswordGenerator = React.lazy(() => import("../PasswordGenerator/PasswordGenerator"));
+const ProjectList = React.lazy(() => import("../Projects/ProjectList"));
+const Swatches = React.lazy(() => import("../Swatches/Swatches"));
+const TimerMain = React.lazy(() => import("../TimerMain/TimerMain"));
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/about" component={Home} />
-      <Route exact path="/abstract-art" component={Abstract} />
-      <Route exact path="/blog" component={Blog} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/css-sandbox" component={CssSandbox} />
-      <Route exact path="/gradient-maker" component={CustomGradient} />
-      <Route exact path="/password-generator" component={PasswordGenerator} />
-      <Route exact path="/projects" component={ProjectList} />
-      <Route exact path="/swatches" component={Swatches} />
-      <Route exact path="/timer" component={TimerMain} />
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={Home} />
+        <Route exact path="/abstract-art" component={Abstract} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/css-sandbox" component={CssSandbox} />
+        <Route exact path="/gradient-maker" component={CustomGradient} />
+        <Route exact path="/password-generator" component={PasswordGenerator} />
+        <Route exact path="/projects" component={ProjectList} />
+        <Route exact path="/swatches" component={Swatches} />
+        <Route exact path="/timer" component={TimerMain} />
+      </Suspense>
     </Router>
   );
 }
