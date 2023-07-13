1dfbaf4c1af2c8b34e36261bf3541d7b4b6f83c6
AwrenNuit <a.rennaker@yahoo.com>
2023-07-13 11:27:05 -0500

    convert about.css to scss; add some color & breakpoint variables

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,11 @@
-import '../stylesheets/about.css';
-
 export default function About() {
   return (
-    <div className="about-main-container">
-      <h2 className="about-heading">
-        <span>A little history...</span>
+    <div className="about">
+      <h2 className="about__heading">
+        <span className="about__heading-copy">A little history...</span>
       </h2>
-      <div className="about-content-container">
-        <p>
+      <div className="about__content-container">
+        <p className="about__content-copy">
           Growing up I was always coming up with new ideas and figuring out how
           to bring them to life. As I got older this manifested through visual
           arts and{' '}
@@ -21,7 +19,7 @@ export default function About() {
           . I performed with the school orchestra all through middle and high
           school, and have taught myself dozens of instruments since then.
         </p>
-        <p>
+        <p className="about__content-copy">
           I also have an affinity for plants, which blossomed into a deep study
           of Western herbalism. Forming a bond with the plants that I use is
           very important to me, so I raise many of them from seed so I can study
@@ -29,7 +27,7 @@ export default function About() {
           to sprout to bloom and back again to seed. Ask me about my homemade
           cough syrup sometime - it's delicious and nutritious!
         </p>
-        <p>
+        <p className="about__content-copy">
           My passions for creating and understanding how things work on a
           fundamental level carry over into my software development. I have a
           profound appreciation for engaging, interactive, and accessible
@@ -37,16 +35,19 @@ export default function About() {
           interaction is an opportunity to learn something new.
         </p>
         <br />
-        <p className="about-contact">
+        <p className="about__contact">
           I'm always open for a conversation, so feel free to&nbsp;
-          <a href="mailto:awren.nuit@gmail.com?subject=Let's%20Have%20a%20Conversation">
+          <a
+            className="u-link about__contact-link"
+            href="mailto:awren.nuit@gmail.com?subject=Let's%20Have%20a%20Conversation"
+          >
             contact me
           </a>
           .
         </p>
-        <span className="about-bottom-line"></span>
-        <span className="about-bottom-design-left"></span>
-        <span className="about-bottom-design-right"></span>
+        <span className="about__bottom-line"></span>
+        <span className="about__bottom-design-left"></span>
+        <span className="about__bottom-design-right"></span>
       </div>
     </div>
   );
