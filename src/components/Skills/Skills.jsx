eceacd6073c18835daa896e922b11361c801d9e7
AwrenNuit <a.rennaker@yahoo.com>
2023-10-09 10:48:12 -0500

    finish Skills updates

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,31 +6,26 @@ export default function Skills() {
   return (
     <div className="skill-main-container">
       <h2 className="skill-heading">
-        <span>These are what I work with the most...</span>
+        <span>Tech I use regularly...</span>
       </h2>
       <div className="skill-flex">
         <div className="skill-group">
-          {/* TODO: map through rowOne */}
-          <SkillContainer imageUrl="images/icons/html.svg" label="HTML" />
-          <SkillContainer imageUrl="images/icons/css.svg" label="CSS" />
-          <SkillContainer imageUrl="images/icons/react.svg" label="React" />
-          <SkillContainer imageUrl="images/icons/vue.png" label="Vue" />
+          {techUsed.rowOne.map((tech, i) => (
+            <SkillContainer
+              key={i}
+              imageUrl={tech.imageUrl}
+              label={tech.label}
+            />
+          ))}
         </div>
         <div className="skill-group">
-          {/* TODO: map through rowTwo */}
-          <SkillContainer imageUrl="images/icons/node.svg" label="Node.js" />
-          <SkillContainer
-            imageUrl="images/icons/rails.png"
-            label="Ruby on Rails"
-          />
-          <SkillContainer
-            imageUrl="images/icons/postgresql.svg"
-            label="PostgreSQL"
-          />
-          <SkillContainer
-            imageUrl="images/icons/firebase.svg"
-            label="Firebase"
-          />
+          {techUsed.rowTwo.map((tech, i) => (
+            <SkillContainer
+              key={i}
+              imageUrl={tech.imageUrl}
+              label={tech.label}
+            />
+          ))}
         </div>
       </div>
     </div>
