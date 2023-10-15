9252f960139b86596623c319f69a9b44a22d0d1d
Awren Nuit <awren.nuit@gmail.com>
2023-10-15 10:42:37 -0500

    add useId to ColorPicker so IDs are unique; update height of CustomGradient

--- src/components/CustomGradient/ColorPicker.jsx
+++ src/components/CustomGradient/ColorPicker.jsx
@@ -1,15 +1,19 @@
+import { useId } from 'react';
+
 export default function ColorPicker({ color, isToolbarOpen, setColor }) {
+  const inputId = useId();
+
   return (
     <div
       className={`color-picker ${
         isToolbarOpen ? 'color-picker--inactive' : ''
       }`}
     >
-      <label className="a11y" htmlFor="color-input">
+      <label className="a11y" htmlFor={`${inputId}-color-input`}>
         Pick a color
       </label>
       <input
-        id="color-input"
+        id={`${inputId}-color-input`}
         className="color-picker__input"
         onChange={(e) => setColor(e.target.value)}
         type="color"
