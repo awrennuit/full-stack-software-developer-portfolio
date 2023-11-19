4b989236ae93c4b14a41f1ed4dd6800fcf71df5d
Awren Nuit <awren.nuit@gmail.com>
2023-11-19 13:38:16 -0600

    set up basic focus trap

--- src/components/Sandbox/Output.jsx
+++ src/components/Sandbox/Output.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export default function Output({
@@ -22,89 +22,100 @@ export default function Output({
   showDialog,
   width,
 }) {
+  const closeRef = useRef(null);
+
   useEffect(() => {
     if (showDialog) {
+      closeRef.current.focus();
       document.body.style.overflow = 'hidden';
       document.addEventListener('keydown', handleEscPress);
-    } else {
+      document.addEventListener('keydown', handleTabPress);
+    }
+
+    return () => {
       document.body.style.overflow = 'auto';
       document.removeEventListener('keydown', handleEscPress);
-    }
+      document.removeEventListener('keydown', handleTabPress);
+    };
   }, [showDialog]);
 
   const handleEscPress = (e) => {
     if (e.key === 'Escape') setShowDialog(!showDialog);
   };
 
-  return (
-    // TODO: trap focus in dialog
-    createPortal(
-      <aside
-        className={`output${showDialog ? ' output--open' : ''}`}
-        onClick={() => setShowDialog(!showDialog)}
+  const handleTabPress = (e) => {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      closeRef.current.focus();
+    }
+  };
+
+  return createPortal(
+    <aside
+      className={`output${showDialog ? ' output--open' : ''}`}
+      onClick={() => setShowDialog(!showDialog)}
+    >
+      <dialog
+        className="output__dialog"
+        aria-labelledby="dialog-title"
+        aria-describedby="dialog-description"
+        onClick={(e) => e.stopPropagation()}
       >
-        <dialog
-          className="output__dialog"
-          aria-labelledby="dialog-title"
-          aria-describedby="dialog-description"
-          onClick={(e) => e.stopPropagation()}
+        <h3 id="dialog-title">Here's Your CSS</h3>
+        <div
+          id="dialog-description"
+          className="output__content"
+          onClick={() => selectText('dialog-description')}
         >
-          <h3 id="dialog-title">Here's Your CSS</h3>
-          <div
-            id="dialog-description"
-            className="output__content"
-            onClick={() => selectText('dialog-description')}
-          >
-            <p>
-              <span>.my-class {'{'}</span>
-              <span>&nbsp;&nbsp;background-color: {bgColor};</span>
-              {+blur !== 0 ? <span>&nbsp;&nbsp;blur: {blur}px;</span> : ''}
-              {+borderWidth !== 0 ? (
-                <span>
-                  &nbsp;&nbsp;border: {borderWidth}px {borderStyle}{' '}
-                  {borderColor};
-                </span>
-              ) : (
-                ''
-              )}
-              {+borderRadius !== 0 ? (
-                <span>
-                  &nbsp;&nbsp;border-radius: {borderRadius}
-                  {borderRadiusType};
-                </span>
-              ) : (
-                ''
-              )}
-              {+boxOffsetX !== 0 ||
-              +boxOffsetY !== 0 ||
-              +boxBlur !== 0 ||
-              +boxSpread !== 0 ? (
-                <span>
-                  &nbsp;&nbsp;box-shadow: {boxInset} {boxOffsetX}px {boxOffsetY}
-                  px {boxBlur}px {boxSpread}px {boxColor};
-                </span>
-              ) : (
-                ''
-              )}
-              <span>&nbsp;&nbsp;height: {height}px;</span>
-              {+rotation !== 0 ? (
-                <span>&nbsp;&nbsp;transform: rotate({rotation}deg);</span>
-              ) : (
-                ''
-              )}
-              <span>&nbsp;&nbsp;width: {width}px;</span>
-              <span>{'}'}</span>
-            </p>
-          </div>
-          <button
-            className="output__btn"
-            onClick={() => setShowDialog(!showDialog)}
-          >
-            Got It
-          </button>
-        </dialog>
-      </aside>,
-      document.body
-    )
+          <p>
+            <span>.my-class {'{'}</span>
+            <span>&nbsp;&nbsp;background-color: {bgColor};</span>
+            {+blur !== 0 ? <span>&nbsp;&nbsp;blur: {blur}px;</span> : ''}
+            {+borderWidth !== 0 ? (
+              <span>
+                &nbsp;&nbsp;border: {borderWidth}px {borderStyle} {borderColor};
+              </span>
+            ) : (
+              ''
+            )}
+            {+borderRadius !== 0 ? (
+              <span>
+                &nbsp;&nbsp;border-radius: {borderRadius}
+                {borderRadiusType};
+              </span>
+            ) : (
+              ''
+            )}
+            {+boxOffsetX !== 0 ||
+            +boxOffsetY !== 0 ||
+            +boxBlur !== 0 ||
+            +boxSpread !== 0 ? (
+              <span>
+                &nbsp;&nbsp;box-shadow: {boxInset} {boxOffsetX}px {boxOffsetY}
+                px {boxBlur}px {boxSpread}px {boxColor};
+              </span>
+            ) : (
+              ''
+            )}
+            <span>&nbsp;&nbsp;height: {height}px;</span>
+            {+rotation !== 0 ? (
+              <span>&nbsp;&nbsp;transform: rotate({rotation}deg);</span>
+            ) : (
+              ''
+            )}
+            <span>&nbsp;&nbsp;width: {width}px;</span>
+            <span>{'}'}</span>
+          </p>
+        </div>
+        <button
+          ref={closeRef}
+          className="output__btn"
+          onClick={() => setShowDialog(!showDialog)}
+        >
+          Got It
+        </button>
+      </dialog>
+    </aside>,
+    document.body
   );
 }
