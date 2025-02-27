7c955871ea0bc2907679656246b9d9e431ba23d4
awren <awren.nuit@gmail.com>
2025-02-26 22:51:19 -0600

    update roll calculations; add more style, theme, and special options

--- src/components/AlbumIdeaGenerator/sections/Instruments.jsx
+++ src/components/AlbumIdeaGenerator/sections/Instruments.jsx
@@ -10,11 +10,13 @@ export default function Instruments({ getRandomNumber }) {
   const rollInstruments = () => {
     const rollOne = getRandomNumber(1, 4);
     const rollTwo = getRandomNumber(1, 4);
+    const rollThree = getRandomNumber(1, 4);
 
     setInstruments({
       rollOne: rollOne,
       rollTwo: rollTwo,
-      total: rollOne + rollTwo + 1,
+      rollThree: rollThree,
+      total: rollOne + rollTwo + rollThree,
     });
   };
 
@@ -22,7 +24,7 @@ export default function Instruments({ getRandomNumber }) {
     <div className="album-idea-generator__card">
       <h2>number of instruments</h2>
       <div className="album-idea-generator__content-top">
-        <p>(2d4 + 1)</p>
+        <p>(3d4)</p>
         <button className="album-idea-generator__btn" onClick={rollInstruments}>
           {!instruments.total ? 'roll' : 'reroll'}
         </button>
@@ -30,7 +32,7 @@ export default function Instruments({ getRandomNumber }) {
       {instruments.total && (
         <p>
           <strong>{instruments.total}</strong> = {instruments.rollOne} +{' '}
-          {instruments.rollTwo} + 1
+          {instruments.rollTwo} + {instruments.rollThree}
         </p>
       )}
     </div>
