eed6f40aa0772e6b2e6ca0e890fbacebfa462998
awren <awren.nuit@gmail.com>
2026-03-20 20:10:13 -0500

    switch to BrowserRouter; create _redirects file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React, { Suspense } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Loading from './components/Loading';
 
 const Abstract = React.lazy(() => import('./components/Abstract'));
-const AlbumIdeaGenerator = React.lazy(() =>
-  import('./components/AlbumIdeaGenerator/AlbumIdeaGenerator')
+const AlbumIdeaGenerator = React.lazy(
+  () => import('./components/AlbumIdeaGenerator/AlbumIdeaGenerator'),
 );
-const AwfHistory = React.lazy(() =>
-  import('./components/AWelcomeFuneral/AwfHistory')
+const AwfHistory = React.lazy(
+  () => import('./components/AWelcomeFuneral/AwfHistory'),
 );
-const AwfPost = React.lazy(() =>
-  import('./components/AWelcomeFuneral/AwfPost')
+const AwfPost = React.lazy(
+  () => import('./components/AWelcomeFuneral/AwfPost'),
 );
 const Contact = React.lazy(() => import('./components/Contact/Contact'));
 const Sandbox = React.lazy(() => import('./components/Sandbox/Sandbox'));
-const CustomGradient = React.lazy(() =>
-  import('./components/CustomGradient/CustomGradient')
+const CustomGradient = React.lazy(
+  () => import('./components/CustomGradient/CustomGradient'),
 );
-const DungeonSynth = React.lazy(() =>
-  import('./components/DungeonSynth/DungeonSynth')
+const DungeonSynth = React.lazy(
+  () => import('./components/DungeonSynth/DungeonSynth'),
 );
-const DungeonVault = React.lazy(() =>
-  import('./components/DungeonSynth/DungeonVault')
+const DungeonVault = React.lazy(
+  () => import('./components/DungeonSynth/DungeonVault'),
 );
 const Home = React.lazy(() => import('./components/Home/Home'));
 const Landing = React.lazy(() => import('./components/Landing'));
 const MyMusic = React.lazy(() => import('./components/MyMusic/MyMusic'));
-const PasswordGenerator = React.lazy(() =>
-  import('./components/PasswordGenerator')
+const PasswordGenerator = React.lazy(
+  () => import('./components/PasswordGenerator'),
 );
-const ProjectNameGenerator = React.lazy(() =>
-  import('./components/ProjectNameGenerator')
+const ProjectNameGenerator = React.lazy(
+  () => import('./components/ProjectNameGenerator'),
 );
 const Projects = React.lazy(() => import('./components/Projects/Projects'));
 const Secret = React.lazy(() => import('./components/Secret'));
