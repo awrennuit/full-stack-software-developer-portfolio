0173f54f199042df47f116a1314b27aa303fc6b7
AwrenNuit <a.rennaker@yahoo.com>
2026-03-27 11:25:40 -0500

    move sass to dev dependencies; add vite build config

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,21 @@ export default defineConfig({
   optimizeDeps: {
     include: ['react', 'react-dom'],
   },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-helmet-async',
+          ],
+        },
+      },
+    },
+  },
 });
