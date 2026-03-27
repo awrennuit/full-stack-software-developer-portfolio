c9f295f9efa749646c10c84fbe9011051eff6bee
AwrenNuit <a.rennaker@yahoo.com>
2026-03-27 09:38:39 -0500

    move timer input list to utils

---
+++ src/utils/timerInputs.js
@@ -0,0 +1,26 @@
+export const timerInputs = [
+  {
+    key: 'days',
+    label: 'Days',
+    max: 99,
+    placeholder: '99',
+  },
+  {
+    key: 'hours',
+    label: 'Hours',
+    max: 23,
+    placeholder: '23',
+  },
+  {
+    key: 'minutes',
+    label: 'Minutes',
+    max: 59,
+    placeholder: '99',
+  },
+  {
+    key: 'seconds',
+    label: 'Seconds',
+    max: 59,
+    placeholder: '59',
+  },
+];
