a28c7651213b5b8132d03b352539b8de744fc4d0
AwrenNuit <a.rennaker@yahoo.com>
2026-03-27 09:20:38 -0500

    update audioFiles

--- src/utils/audioFiles.js
+++ src/utils/audioFiles.js
@@ -1,6 +1,3 @@
-const audioFiles = [
-  { src: 'music/moth.mp3', title: 'Moth' },
-  { src: 'music/a-darkness.mp3', title: 'A Darkness Falls' },
-];
+const audioFiles = [{ src: 'music/moth.mp3', title: 'Moth' }];
 
 export { audioFiles };
